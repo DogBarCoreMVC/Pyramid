14d4f15a7f6b579369f8d1996b0d5948f8b3ff69
GitDogBar <Dogbar>
2021-08-10 15:01:38 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string continus = "yes";
+            while (continus == "yes")
+            {
+
+                Console.Write("Enter the desired height (number):");
+                int userInput = (int)double.Parse(Console.ReadLine());
+                string outPut = "";
+                while (userInput > 0)
+                {
+                    outPut += "*";
+                    Console.WriteLine(outPut);
+                    userInput--;
+                }
+                Console.Write("Continus (yes/no):");
+                continus = Console.ReadLine();
+            }
+            Console.WriteLine("Good Bye");
+        }
+    }
+}
